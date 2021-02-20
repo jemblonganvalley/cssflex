ec2f1b3ed44a1b289e2c4a6ac44b26cba3f4c6d4
jemblonganvalley <jemblonganvalley@gmail.com>
2021-02-20 13:43:14 +0700

    update css flex

---
+++ main.js
@@ -0,0 +1,9 @@
+//mengatur css display
+const cssDisplay = ()=>{
+    //tangkap select value
+    let selectValue = document.getElementById("css_display").value
+
+    //tampilkan di log
+    console.log(selectValue);
+    
+}
